655b6b367930008ffa609dd2b50656e73d13436b
senseiTumelo <tmotjopi@gmail.com>
2021-10-26 11:48:18 +0200

    same function 0.1

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -4,12 +4,14 @@ function seed() {
 
 
 function same([x, y], [j, k]) {
-  return x === y && j === k;
+  return x === j && y === k;
 }
 
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+
+}
 
 const printCell = (cell, state) => {};
 
