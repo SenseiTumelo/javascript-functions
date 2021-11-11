b56a2d7ac2340e10335cf60fee5723cf6656dcdd
senseiTumelo <tmotjopi@gmail.com>
2021-11-11 11:11:34 +0200

    main

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -65,11 +65,30 @@ const willBeAlive = (cell, state) => {
   ))
 };
 
-const calculateNext = (state) => {};
+const calculateNext = (state) => {
+  const { bottomLeft, topRight } = corners(state);
+  let result = [];
+  for (let y = topRight[1] + 1; y >= bottomLeft[1]; y--){
+    for (let x = bottomLeft[0] - 1; x <= topRight[0]; x++){
+      result = result.concat(willBeAlive([]))
+    }
+  }
+};
 
-const iterate = (state, iterations) => {};
+const iterate = (state, iterations) => {
+   const states = [state];
+   for (let i = 0; i< iterations; i++) {
+     states.push(calculateNext(states[states.length - 1]));
+     return states;
+   }
 
-const main = (pattern, iterations) => {};
+};
+
+const main = (pattern, iterations) => {
+const results = iterate(startPatterns[pattern],iterations);
+results.forEach( r => console.log(printCells(r)));
+
+};
 
 const startPatterns = {
     rpentomino: [
