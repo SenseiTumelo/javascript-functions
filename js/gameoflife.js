b7c510dd942a9d9f494851f673c71dc6937acc98
senseiTumelo <tmotjopi@gmail.com>
2021-11-11 11:32:42 +0200

    fixed iteration() bugs

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -80,9 +80,9 @@ const iterate = (state, iterations) => {
    const states = [state];
    for (let i = 0; i< iterations; i++) {
      states.push(calculateNext(states[states.length - 1]));
-     return states;
+     
    }
-
+   return states;
 };
 
 const main = (pattern, iterations) => {
